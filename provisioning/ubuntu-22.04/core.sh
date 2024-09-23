789b4efd58a96c2291028ade17c26263ca200366
pharr117 <pjharral@gmail.com>
2024-09-23 12:42:30 -0400

    Add superfluous second apt update that seems to fix sometimes missing build-essential on GCP

--- provisioning/ubuntu-22.04/core.sh
+++ provisioning/ubuntu-22.04/core.sh
@@ -5,4 +5,5 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # provisioner pre-requisites
+sudo apt-get update # sometimes on GCP, the build-essental package is not found, and this seems to fix it even though its already run above
 sudo apt-get install -y git curl apt-transport-https ca-certificates gnupg wget sudo openssh-server openssh-client build-essential
